616c6a501d2a27fe772fd5133564bab4052a36ec
Anshumaan Singh <anshumaanborntolead@gmail.com>
2021-04-28 18:08:04 +0530

    portal added

--- report.php
+++ report.php
@@ -1,8 +1,13 @@
-<html>
-    <head>  
-        <title>Reaction Timer Tester Game</title>
+<?php
+header("X-XSS-Protection: 1; mode=block");
 
-        <style type="text/css">
+session_start();
+?>
+<html>
+<head>
+<title>Reaction Timer Tester Game</title>
+
+    <style type="text/css">
             
             body {
                 
@@ -27,12 +32,29 @@
             }
         
         </style>
-    </head>
-    <body>
-        <h1>Test Your Reactions!</h1>
+    
+</head>
+<body>
+
+<?php
+    error_reporting(0);
+
+if($_SESSION["username"]) {
+?>
+Welcome <?php echo htmlspecialchars($_SESSION["username"]); ?>. Click here to <a href="logout.php" title="Logout">Logout.</a>
+    
+    
+    <h1>Test Your Reactions!</h1>
         <p>Click on the boxes and circles as quickly as you can!</p>
         <p class="bold">Your time: <span id="timeTaken"></span></p>
         <div id="shape"></div>
      <script type="text/javascript" src="effects.js"> </script>
-    </body>
+   
+    
+    
+<?php
+}else header("Location:index.php");
+?>
+</body>
 </html>
+
